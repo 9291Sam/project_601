abd1b76a7177a9f8f91e7f275ce1381abc12334c
Sam Skinner <sebs@Sams-MacBook-Pro-3.local>
2025-12-12 20:27:28 -0500

    sync

--- intro.m
+++ intro.m
@@ -1,19 +1,34 @@
 %% Simulate the system starting at the unstable equilibrium
 % clc; clear; close all;
-% sys = ballbot();
+sys = ballbot();
 
-% % initial condition (vertical)
-% x0 = [0; 0; 0.001; 0.001; 0; 0; 0; 0];
-% % u = [0; 0]
+% initial condition (vertical)
+x0 = [0; 0; 0.00001; 0.0001; 0; 0; 0; 0];
+% u = [0; 0]
 
-% % time vector
-% t = 0:0.1:60;
+% time vector
+t = 0:0.01:15;
 
-% % simulate the system
-% x = sys.simulate(t,x0);
+% simulate the system
+x = sys.simulate(t,x0);
 
-% % visualize the dynamics
+% visualize the dynamics
 % sys.draw(x);
+
+
+s = 1
+
+figure("Position", [100, 100, 1280, 720]);
+set(gcf, 'Color', 'w');
+% refs = r(t_span);
+plot(t, x(:, 7)', "r"); hold on
+% plot(t_span, refs(s, :), 'b'); hold on
+% plot(t, x(:, 3), 'g'); hold on
+
+legend("dottheta")
+exportgraphics(gcf, 'uncontrolled_dottheta_no_deviation.png'); 
+
+
 
 %% Control via lqr
 
@@ -81,61 +96,61 @@
 % % visualize the dynamics
 % sys.draw(x);
 %% control from observer
-sys = ballbot();
+% sys = ballbot();
 
-% time vector
-linearization_x = [3; 2; 0; 0; 0; 0; 0; 0];
-utilde = [0; 0];
+% % time vector
+% linearization_x = [3; 2; 0; 0; 0; 0; 0; 0];
+% utilde = [0; 0];
 
-% initial condition 
-x0_plant = [-3; 0; 0.5; -.6; 0; 0; 0; 0];
-x0_observer = zeros(size(x0_plant));
+% % initial condition 
+% x0_plant = [-3; -1.33; 0.5; -.6; 0; 0; 0; 0];
+% x0_observer = zeros(size(x0_plant));
 
-% x0 = [x0_plant; x0_plant - [0; 0; 0 - 0.1; 0; 0; 0; 0; 0]];
-x0 = [x0_plant; x0_observer];
+% % x0 = [x0_plant; x0_plant - [0; 0; 0 - 0.1; 0; 0; 0; 0; 0]];
+% x0 = [x0_plant; x0_observer];
 
-radius = 3.0;
-period = 5.0;
-w = 2 * pi / period;
-t_span = 0:0.025:22;
-r = @(t) [
-    radius * cos(w*t);       
-    radius/2 * sin(2*w*t);     
-    0; 
-    0;
-    0;
-    0;
-    % -radius * w * sin(w*t);
-    % radius * w * cos(2*w*t);
-    0;
-    0 
-];
+% radius = 3.0;
+% period = 5.0;
+% w = 2 * pi / period;
+% t_span = 0:0.025:22;
+% r = @(t) [
+%     radius * cos(w*t);       
+%     radius/2 * sin(2*w*t);     
+%     0*t; 
+%     0*t;
+%     0*t;
+%     0*t;
+%     0*t;
+%     0*t 
+% ];
 
-% linearization
-[A,B] = sys.linearize(linearization_x,utilde);
+% % linearization
+% [A,B] = sys.linearize(linearization_x,utilde);
 
-% controller
-[K, ~, ~] = lqr(A, B, diag([10, 10, 1, 1, 1, 1, 1, 1]), eye(2));
+% % controller
+% [K, ~, ~] = lqr(A, B, diag([10, 10, 1, 1, 1, 1, 1, 1]), eye(2));
 
-C = [
-    1, 0, 0, 0, 0, 0, 0, 0;
-    0, 1, 0, 0, 0, 0, 0, 0;
-    % 0, 0, 1, 0, 0, 0, 0, 0;
-    % 0, 0, 0, 1, 0, 0, 0, 0;
-    % 0, 0, 0, 0, 1, 0, 0, 0;
-    % 0, 0, 0, 0, 0, 1, 0, 0;
-    0, 0, 0, 0, 0, 0, 1, 0;
-    0, 0, 0, 0, 0, 0, 0, 1;
-];
-[rows, columns] = size(C);
-D = zeros(rows, 2);
+% C = [
+%     1, 0, 0, 0, 0, 0, 0, 0;
+%     0, 1, 0, 0, 0, 0, 0, 0;
+%     % 0, 0, 1, 0, 0, 0, 0, 0;
+%     % 0, 0, 0, 1, 0, 0, 0, 0;
+%     % 0, 0, 0, 0, 1, 0, 0, 0;
+%     % 0, 0, 0, 0, 0, 1, 0, 0;
+%     0, 0, 0, 0, 0, 0, 1, 0;
+%     0, 0, 0, 0, 0, 0, 0, 1;
+% ];
+% [rows, columns] = size(C);
+% D = zeros(rows, 2);
 
-[L_T, ~, ~] = lqr(A', C', 10 * diag([1, 1, 25, 25, 1, 1, 5, 5]), eye(rows));
-L = L_T';
+% [L_T, ~, ~] = lqr(A', C', 10 * diag([1, 1, 25, 25, 1, 1, 5, 5]), eye(rows));
+% L = L_T';
 
 
-% simulate the system
-x = sys.observed_trajectory_tracked(t_span,x0,K,r, L, C, D);
+% % simulate the system
+% x = sys.observed_trajectory_tracked(t_span,x0,K,r, L, C, D);
+
+ 
 
 % visualize the dynamics
-sys.draw(x);
+% sys.draw(x);
